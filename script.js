51658e96197041e0e4e88ddb67ec41abbd304ac0
leeroynico <73426065+leeroynico@users.noreply.github.com>
2021-05-21 00:12:27 +0200

    had to fix start again

--- script.js
+++ script.js
@@ -5,28 +5,40 @@ const talk = new SpeechSynthesisUtterance();
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-start.addEventListener("click", () => {
-  intervalId = setInterval(speak, 2000);
-});
 
-let intervalles = 500;
-let repetition = 3;
+let intervalles = 1000;
+let timeToTrain = 8000;
 let intervalId = null;
+
 function speak() {
   window.speechSynthesis.speak(talk);
   talk.text = getRandomArbitrary(1, 7);
   const voices = window.speechSynthesis.getVoices();
-  //talk.voice = voices[4];
   talk.volume = 1; //accepts between [0 - 1], defaults to 1
   talk.rate = 5; //accepts between [0.1 - 10], defaults to 1
   talk.pitch = 1.5; //accepts between [0 - 2], defaults to 1
 }
-function startTrain() {
-  intervalId = setInterval(speak, 1000);
+function warning(message) {
+  window.speechSynthesis.speak(talk);
+  talk.text = message;
+  const voices = window.speechSynthesis.getVoices();
+  talk.volume = 1; //accepts between [0 - 1], defaults to 1
+  talk.rate = 5; //accepts between [0.1 - 10], defaults to 1
+  talk.pitch = 1.5; //accepts between [0 - 2], defaults to 1
+}
+function annonceFin() {
+  warning("tranquille");
 }
 function stopTrain() {
   clearInterval(intervalId);
 }
+start.addEventListener("click", (e) => {
+  warning("attention ça va démarrer");
+  intervalId = setInterval(speak, intervalles);
+  setTimeout(stopTrain, timeToTrain);
+  setTimeout(annonceFin, timeToTrain + 1500);
+});
+
 stopit.addEventListener("click", () => {
   stopTrain();
 });
